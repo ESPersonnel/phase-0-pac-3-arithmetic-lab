0afe4a7783043df7db4b662ef20a0d675073d6fd
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-05 14:25:50 +0300

    commit tree 8a56e2772ee0a6700b52d3c894039855056dd85f

---
+++ index.js
@@ -0,0 +1,39 @@
+// Addition
+function add(a, b) {
+    return a + b
+}
+
+// Subtraction
+function subtract(a, b) {
+    return a - b
+}
+
+// Multiplication
+function multiply(a, b) {
+    return a * b
+}
+
+// Division
+function divide(a, b) {
+    return a / b
+}
+
+// Increment
+function increment(n) {
+    return ++n
+}
+
+// Decrement
+function decrement(n) {
+    return --n
+}
+
+// makeInt  
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+// preserveDecimal
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
